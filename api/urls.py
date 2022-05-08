ec549160f7bf0a74d33358f1f91b5d8ac38cdfb0
Almas <aalmas2001@gmail.com>
2022-05-08 18:36:18 +0600

    urls updated

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,12 @@ from rest_framework_simplejwt.views import (
 )
 
 from api.views.function_based_views.cart import *
+from api.views.function_based_views.guidebook import *
+from api.views.class_based_views.sign_up import SignUpView
 from api.views.class_based_views.product import *
+from api.views.class_based_views.category import *
+from api.views.class_based_views.user import *
+
 
 router = routers.DefaultRouter()
 
@@ -16,6 +21,7 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token-verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('sign-up/', SignUpView.as_view()),
 
     path('carts/', cart_list),
     path('carts/<int:cart_id>/', cart_details),
@@ -23,7 +29,17 @@ urlpatterns = [
 
     path('products/', ProductListAPIView.as_view()),
     path('products/<int:product_id>', ProductDetailsAPIView.as_view()),
-    path('products/<str:category_name>', ProductByCategoryAPIView.as_view()),
+    path('products/<str:category_name>', CategoryProductsAPIView.as_view()),
 
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<str:name>/', CategoryDetailsAPIView.as_view()),
+    path('categories/<str:name>/products/', CategoryProductsAPIView.as_view()),
 
+    path('users/', UserListAPIView.as_view()),
+    path('users/<int:user_id>/', UserDetailsAPIView.as_view()),
+    path('users/<int:user_id>/cart/', CartByUserAPIView.as_view()),
+
+    path('guidebooks/', guidebook_list),
+    path('guidebooks/<int:guidebook_id>/', guidebook_details),
+    path('guidebook/<int:guidebook_id>/content/', guidebook_content),
 ]
